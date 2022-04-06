446e162a0562d2177df099dd997632fa3a7cc558
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-06 16:43:00 +0200

    creato nuovo progetto utilizzando Vue CLI ed aggiunti due componenti, uno per l'header e l'altro per il main

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Main/>
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display:flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+      
+  </main>
+</template>
+
+<script>
+export default {
+    name:'MainComponent'
+}
+</script>
+
+<style>
+
+</style>
