9053e6844bdd823981906d901b16df419a4b7b87
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-07 16:05:44 +0200

    modifica al css tramite sintassi scss

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,9 @@
 <template>
   <main>
-      <div class="container">(
-        <div class="thumb" v-for="(poster,index) in posters" :key="'poster-' + index">
+      <div class="container">
+        <div class="thumb" v-for="(poster,index) in posters" :key="'poster-' + index">  <!--poster mi rappresenta il singolo oggetto dell'array passato tramite props-->
           <img :src="poster.thumb" :alt="poster.thumb">
+          <div class="text">{{poster.series}}</div>
         </div>
       </div>
   </main>
@@ -21,10 +22,15 @@ export default {
   .container{
      max-width: 1200px;
      margin: 0 auto;
+     background-color: #1c1c1c;
      display: flex;
     flex-wrap: wrap;
     .thumb{
       width:calc(100% / 6);
+      .text{
+        color:white;
+        font-size:20px;
+      }
     }
   }
 </style>
